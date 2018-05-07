25e8e0c7d4f4f86ffc5257dd78768bb36a6f9219
Akila Wickey <akilawickey@Akilas-MacBook-Air.local>
2018-05-07 22:18:57 +0530

    basic node config

---
+++ config.js
@@ -0,0 +1,22 @@
+//Application Configuration
+'use strict'
+
+var config = module.exports;
+
+//your test platform1 configs
+
+config.db1 = {
+	user: 'root',
+	password: 'root',
+	name: 'database1'
+};
+
+//your test platform2 configs
+
+config.db2 = {
+	user: 'akila',
+	password: 'danny',
+	name: 'dbpwd'
+};
+
+
